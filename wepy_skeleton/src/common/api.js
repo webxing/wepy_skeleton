6b0c8e82a2d140151e8ae4250468afeeb55b227a
lx <chanpinliuxin@kaochong.com>
2019-06-12 11:27:05 +0800

    授权与登录流程

--- wepy_skeleton/src/common/api.js
+++ wepy_skeleton/src/common/api.js
@@ -3,10 +3,24 @@ import http from './http'
 
 // 获取openID
 function getOpenId(data) {
-  return http.get('/getopenid', data)
+  // return http.get('/getopenid', data)
+
+  // 模拟返回数据
+  return 'oze5c5FdnX3w3StH2c9KcTb0fuSk'
+}
+
+// 登录
+function login(data) {
+  // return http.get('/login', data)
+
+  // 模拟返回数据
+  return {
+    token: "9663de0677fb4992ac56817cfe26f9dc"
+  }
 }
 
 
 export default {
-  getOpenId
+  getOpenId,
+  login
 }
